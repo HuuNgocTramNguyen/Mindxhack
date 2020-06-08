58ac9a8e456b212b83aaa3a8fd985afaa62524b4
NguyenHuuNgocTram <1754050094tram@ou.edu.vn>
2020-06-08 21:10:19 +0700

    hahaha

---
+++ js/html.js
@@ -0,0 +1,11 @@
+let signin = document.getElementById("sign-in");
+signin.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "../html/signin.html"
+});
+
+let signup = document.getElementById("sign-up");
+signup.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "../html/signin02.html"
+});
